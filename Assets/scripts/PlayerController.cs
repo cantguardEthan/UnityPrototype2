0a95d443932d35dfffa6b178114736649f4ececd
cantguardEthan <ethanmatungiro@gmail.com>
2024-10-07 13:25:12 -0400

    changes

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+     public float horizontalInput;
+     public float speed = 10.0f;
+     public float xRange = 10.0f;
+
+     public GameObject projectileprefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+         if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectileprefab, transform.position, projectileprefab.transform.rotation);
+        }
     }
 }
